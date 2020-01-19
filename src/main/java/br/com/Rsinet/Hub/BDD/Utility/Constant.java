a532db62eceed506de5bb257933ac06a31388008
Joice Natalice Alves De Abreu <joice.abreu@rsinet.com.br>
2020-01-19 19:34:50 -0300

    Adicionando as Evidencias, como Excel, PrintdeTela e a Constants

---
+++ src/main/java/br/com/Rsinet/Hub/BDD/Utility/Constant.java
@@ -0,0 +1,12 @@
+package br.com.Rsinet.Hub.BDD.Utility;
+
+public class Constant {
+
+	public static final String URL = "https://www.advantageonlineshopping.com/#/";
+
+	public static final String ProdutoPesquisado = "HP ZBOOK 17 G2 MOBILE WORKSTATION";
+	
+	public static final String Path_TestData = "C:\\Users\\joice.abreu\\eclipse-workspace\\br.com.Rsinet.Hub.BDD\\Evidencias\\Excel";
+
+	public static final String File_TestData = "DadosParaCadastro.xlsx";
+}
